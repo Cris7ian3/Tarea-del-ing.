c5cb02bb77c4af1f5aa274744a63394b943a937d
cris7ian3 <cristrojas1e@email>
2024-11-11 00:22:25 -0400

    tarea del ingeniero

---
+++ Tarea del inge Tic Tac Toe/src/TicTacToeGUI.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToeGUI extends JFrame implements ActionListener {
+    private JButton[][] buttons = new JButton[3][3];
+    private TicTacToeGame game;
+
+    public TicTacToeGUI() {
+        game = new TicTacToeGame();
+        setTitle("Tic Tac Toe");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col] = new JButton("");
+                buttons[row][col].setFont(new Font("Arial", Font.PLAIN, 60));
+                buttons[row][col].setFocusPainted(false);
+                buttons[row][col].addActionListener(this);
+                add(buttons[row][col]);
+            }
+        }
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+        int row = -1, col = -1;
+
+        // Find which button was clicked
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (clickedButton == buttons[i][j]) {
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+        }
+
+        // Make a move
+        if (row != -1 && col != -1 && game.makeMove(row, col)) {
+            clickedButton.setText(String.valueOf(game.getCurrentPlayer()));
+
+            if (game.checkForWin()) {
+                JOptionPane.showMessageDialog(this, "Player " + game.getCurrentPlayer() + " wins!");
+                game.resetBoard();
+                resetGUI();
+            } else if (game.isBoardFull()) {
+                JOptionPane.showMessageDialog(this, "It's a tie!");
+                game.resetBoard();
+                resetGUI();
+            } else {
+                game.switchPlayer();
+            }
+        }
+    }
+
+    private void resetGUI() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col].setText("");
+            }
+        }
+    }
+}
---
+++ Tarea del inge Tic Tac Toe/src/TicTacToeGame.java
@@ -0,0 +1,61 @@
+public class TicTacToeGame {
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToeGame() {
+        board = new char[3][3];
+        resetBoard();
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    public boolean makeMove(int row, int col) {
+        if (board[row][col] == ' ') {
+            board[row][col] = currentPlayer;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkForWin() {
+        for (int i = 0; i < 3; i++) {
+            // Check rows and columns
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer)
+                return true;
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)
+                return true;
+        }
+        // Check diagonals
+        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer)
+            return true;
+        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)
+            return true;
+        return false;
+    }
+
+    public boolean isBoardFull() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void resetBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = ' ';
+            }
+        }
+        currentPlayer = 'X';
+    }
+}
